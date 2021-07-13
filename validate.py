213ffa3911d779a2f64e8905b3367550f30887c3
Martin Hübner <martin.hubner@web.de>
2021-07-13 16:54:45 +0200

    Amendments to json-schema, add validator-script.

---
+++ validate.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+# validate json-schema. Include format-checker for email and ip-address checks.
+
+import jsonschema
+import json
+import sys
+
+if len(sys.argv) != 3:
+    print("usage:\t./validate.py $JSON_FILE $JSON_SCHEMA")
+    exit(1)
+
+string = json.load(open(sys.argv[1]))
+schema = json.load(open(sys.argv[2]))
+
+jsonschema.validate(
+    instance=string,
+    schema=schema,
+    format_checker=jsonschema.draft7_format_checker,
+)
+
+print("The JSON-String looks like it's valid. :)")
